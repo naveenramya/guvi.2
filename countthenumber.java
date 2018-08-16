26665b6406599cae5c62ee45c9aa6b87df340cd1
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-16 12:53:37 +0530

    Create countthenumber.java

---
+++ countthenumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s;
+		int a=0;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)==' ')
+			{
+				continue;
+			}
+			a++;
+		}
+		System.out.println(a);
+		
+		
+	}
+}
+
